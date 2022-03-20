1e104c1858b43ce9278b9684b2ff424ed3056e44
minji0123 <sso02027@naver.com>
2022-03-20 14:21:54 +0900

    AWS 에 서비스 배포(back,front)

--- todo-react-app/src/app-config.js
+++ todo-react-app/src/app-config.js
@@ -2,8 +2,11 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if (hostname == "localhost"){
-    backendHost = "http://localhost:8080";
+if (hostname === "localhost"){
+   backendHost = "http://localhost:8080";
+}else{
+    backendHost = "http://prod-todo-backend.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
+
